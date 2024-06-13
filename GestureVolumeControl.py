8bbbf0483b3d6c5ba220d40d1ca1a60f3a10f7fb
prnvvv <prannavakhanth12@gmail.com>
2024-06-13 14:31:36 +0530

    New changes

--- GestureVolumeControl.py
+++ GestureVolumeControl.py
@@ -1,39 +1,50 @@
 import cv2
 import numpy as np
 import time
-import HandDetectorModule as hdm
+import HandTrackingModule as htm
 
+# Initialize webcam capture
 capture = cv2.VideoCapture(0)
 
+# Set video width and height
 capture.set(3, 1280)
 capture.set(4, 720)
 
-detector = hdm.HandDetector()
+# Initialize hand detector
+detector = htm.HandDetector()
 
-if not capture.isOpened:
+# Check if webcam is opened successfully
+if not capture.isOpened():
     raise Exception("Webcam is not opened")
 
+# Initialize time variables for FPS calculation
 currentTime = previousTime = 0
 
 while True:
+    # Capture frame-by-frame
     success, vidObject = capture.read()
-
-    detector.detectHands()
 
     if not success:
         raise Exception("Error while loading the Webcam")
-    
+
+    # Detect hands in the frame
+    vidObject = detector.detectHands(vidObject)
+
+    # Calculate FPS
     currentTime = time.time()
     fps = 1 / (currentTime - previousTime)
     previousTime = currentTime
 
+    # Display FPS on the video
     cv2.putText(vidObject, f"FPS : {int(fps)}", (40, 70), cv2.FONT_ITALIC, 2, (255, 0, 0), 2)
+
+    # Show the video
     cv2.imshow("Video", vidObject)
 
-    if cv2.waitKey(1) & 0x00FF == ord("q"):
+    # Break the loop when 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 print("Exiting...")
 capture.release()
 cv2.destroyAllWindows()
-        
